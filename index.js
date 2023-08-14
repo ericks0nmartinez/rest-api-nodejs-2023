84fa232261def9a905e37644c6ed5f9163da26b1
Erickson De Souza Martinez <erickson.martinez@meta.com.br>
2023-08-14 00:18:25 -0300

    feat: query params in the end point

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ console.log("App init");
 server.get("/", (req, res) => {
   return res.send({ name: "App init", version: "1.0.0" });
 });
+
+server.get("/query-params", (req, res) => {
+  const { name, lang } = req.query;
+  return res.send({
+    result: lang === "pt" ? `Seja bem vindo ${name}` : `Welcome ${name}`,
+  });
+});
